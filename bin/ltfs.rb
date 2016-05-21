64a6a1d9f3d4874092382f5324f9764c044a1488
bradcordeiro <me@bradc.com>
2016-05-21 11:44:30 +1000

    Add some documentation to classes and methods

--- bin/ltfs.rb
+++ bin/ltfs.rb
@@ -1,9 +1,14 @@
 require 'xmlsimple'
 
+# Class representing the entire LTFS index schema. Tape information is stored on initialization but because
+# parsing file information is much more resource-heavy, file information is not parsed until the
+# first time the .files method is called.
 class LTFSIndex
 
 	def initialize(file)
-		@path_components = ["/"]
+		# Folder stucture in XML is represented by nesting, @path_components stores the current path
+    # during recursive parsing of folders
+    @path_components = ["/"]
 		@array_of_ltfsfiles = []
 		@tapedata = LTFSTape.new(XmlSimple.xml_in(file, 'ForceArray' => false, 'KeyToSymbol' => true))
 		@filedata = XmlSimple.xml_in(file, 'ForceArray' => false, 'KeyToSymbol' => true)[:directory][:contents]
@@ -14,16 +19,20 @@ class LTFSIndex
   end
 
   private
+    # Find XML representing files and folders, and pass it to the appropriate method
   	def parse(subdata)
   		handle_files(subdata[:file]) if subdata.has_key?(:file)
   		handle_folders(subdata[:directory]) if subdata.has_key?(:directory)
   	end
   	
+    # Traverse deeper into XML representing a folder to parse its contents
     def handle_folders(subdata)
-		  if subdata.is_a? Hash
+		  # Single folder
+      if subdata.is_a? Hash
 			  @path_components.push(subdata[:name])
 		  	parse(subdata[:contents])
 		  	@path_components.slice!(-1)
+        # Multiple folders
 		  elsif subdata.is_a? Array
 		  	subdata.each do |f|
 		  		@path_components.push(f[:name])
@@ -32,19 +41,21 @@ class LTFSIndex
 		  	end
 		  end
     end
-
-  def handle_files(subdata)
-		# single file in a folder
-		if subdata.is_a? Hash
-			@array_of_ltfsfiles.push(LTFSFile.new(subdata, @tapedata.volumeuuid, @path_components))
-		# multiple files in a folder
-		elsif subdata.is_a? Array
-			subdata.each { |f| @array_of_ltfsfiles.push(LTFSFile.new(f, @tapedata.volumeuuid, @path_components)) }
-		end
-  end
+  
+    # Parse XML respresenting a file on tape into an LTFSFile object, and add it to an array of files
+    def handle_files(subdata)
+  		# single file in a folder
+  		if subdata.is_a? Hash
+  			@array_of_ltfsfiles.push(LTFSFile.new(subdata, @tapedata.volumeuuid, @path_components))
+  		# multiple files in a folder
+  		elsif subdata.is_a? Array
+  			subdata.each { |f| @array_of_ltfsfiles.push(LTFSFile.new(f, @tapedata.volumeuuid, @path_components)) }
+  		end
+    end
 
 end
 
+# Represents overall information about a tape
 class LTFSTape
 	attr_reader :version, :creator, :volumeuuid, :generation, :updatetime, 
 							:indexpartition, :indexstartblock, :previousgenerationpartition, 
@@ -68,6 +79,7 @@ class LTFSTape
 
 end
 
+# Represents a file in an LTFS schema
 class LTFSFile
 	attr_reader :filename, :length, :editable, :creationtime, :changetime,
     	        :modifytime, :accesstime, :backuptime, :fileuid, :filepath,
@@ -86,10 +98,6 @@ class LTFSFile
 		@fileuid = input[:fileuid]
 		if input[:length].nil? then @length = 0 else @length = input[:length] end
 		@extentinfo = LTFSExtentInfo.new(input[:extentinfo])
-	end
-	
-	def to_s
-	  File.join(@filepath, @filename)
 	end
 	
 	def fileoffset
@@ -114,6 +122,8 @@ class LTFSFile
 
 end
 
+# Poor tape-writing practice causes large variations in Extent information about an LTFSFile, so this
+# part of information has been abstracted to its own class to do some sanity checks.
 class LTFSExtentInfo
 
   attr_reader :bytecount, :byteoffset, :startblock, :fileoffset, :partition
@@ -144,7 +154,8 @@ class LTFSExtentInfo
       @bytecount, @byteoffset, @startblock, @fileoffset, @partition = 0, 0, 0, 0, 'b'
     end
     
-    # Poorly written tapes will have multiple values for the same field
+    # Poorly written tapes will have multiple values for the same field, so here we create a
+    # comma-separated string of all of the values.
     def handle_extent_array
       bytecounts, byteoffsets, startblocks, fileoffsets, partitions = [], [], [], [], []
       @extentinfo[:extent].each do |f|
@@ -158,7 +169,7 @@ class LTFSExtentInfo
       @byteoffset = byteoffsets.join(',')
       @startblock = startblocks.join(',')
       @fileoffset = fileoffsets.join(',')
-      @partition = partitions.join('.')
+      @partition = partitions.join(',')
     end
 
-end∑
+end
